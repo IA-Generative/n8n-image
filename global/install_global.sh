bdedd1eba0113a57662a7abf918258de009f4985
ArnaudTa <33383276+ArnaudTA@users.noreply.github.com>
2026-01-21 15:29:10 +0100

    refactor: improve global package installation script and Dockerfile structure

--- global/install_global.sh
+++ global/install_global.sh
@@ -5,4 +5,6 @@ set -euo pipefail
 # parse the package.json to get the list of dependencies and install them globally
 # respect version numbers
 # install all depencies in one command 
-npm install -g $(jq -r '.dependencies | to_entries | map("\(.key)@\(.value)") | join(" ")' package.json)
+PACKAGES=$(jq -r '.dependencies | to_entries | map("\(.key)@\(.value)") | join(" ")' package.json)
+echo "Installing global packages: $PACKAGES"
+npm install -g $PACKAGES
